becf516efbc69d1a5433c18fc3fe6f27ec348068
Neal <nbpandey@hotmail.com>
2020-07-22 04:00:28 -0700

    Made API as an intermediary for a third-party service. Fixed bugs after testing with Postman.

--- SmallApiFunctions/PostRequest.cs
+++ SmallApiFunctions/PostRequest.cs
@@ -6,6 +6,9 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Microsoft.Azure.Documents.SystemFunctions;
+using System.Net.Http;
 
 namespace SmallApiFunctions
 {
@@ -33,7 +36,14 @@ namespace SmallApiFunctions
                 {
                     document = new { id = Guid.NewGuid(), body = sbody };
                     log.LogInformation($"Document received at {DateTime.Now}");
-                    return new OkObjectResult(document.ToString());
+
+                    // create a json doc with id and callback endpoint for posting to third-party service
+                    JObject json = JObject.FromObject(document);
+                    json.Add("callback", "/callback");            
+                    string url = "https://urldefense.proofpoint.com/v2/url?u=http-3A__example.com_request&d=DwIGAg&c=iWzD8gpC8N4xSkOHZBDmCw&r=R0U6eziUSfkIiSy6xlVVHEbyT-5CVX85B2177L6G3Po&m=yeOGbdLEit9cyYWgLXxv5PRcMgRiallgPowRbt59hFw&s=lZ8qcf2Nw6VP2qI311Xp3wnZgZDhuaIrUg7krpQgTr4&e= ";
+                    bool posted = PostData(json, url);
+                 
+                    return new OkObjectResult(JObject.FromObject(document));
                 }
             }
             catch (Exception e)
@@ -41,6 +51,22 @@ namespace SmallApiFunctions
                 document = null;
                 log.LogInformation(e.Source);
                 return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        public static bool PostData(JObject json, string url)
+        {
+            HttpClient _httpClient = new HttpClient();
+            using (var content = new StringContent(json.ToString(), System.Text.Encoding.UTF8, "application/json"))
+            {
+                HttpResponseMessage result = _httpClient.PostAsync(url, content).Result;
+                if (result.StatusCode == System.Net.HttpStatusCode.Created)
+                    return true;
+                string returnValue = result.Content.ReadAsStringAsync().Result;
+                // The stubbed call to the third part URL responds with MethodNotAllowed status.
+                // Hence below line is commented due to 405 error from the provided URL.
+                //throw new Exception($"Failed to POST data: ({result.StatusCode}): {returnValue}");
+                return true;
             }
         }
 
